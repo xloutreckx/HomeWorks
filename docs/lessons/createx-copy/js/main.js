bce946cacb67e1096bd8078dc44a7deacdab1751
xloutreckx <133657705+xloutreckx@users.noreply.github.com>
2023-06-05 23:41:38 +1000

    Add files via upload

--- docs/lessons/createx-copy/js/main.js
+++ docs/lessons/createx-copy/js/main.js
@@ -1,4 +1,15 @@
 
+$(function () {
+
+  var mixer = mixitup('.direction__items');
+
+  $('.direction__filter-btn').on('click', function () {
+  $('.direction__filter-btn').removeClass('direction__filter-btn--active')
+  $(this).addClass('direction__filter-btn--active')
+  })
+
+})
+
 Fancybox.bind("[data-fancybox]", {
   src: "https://youtu.be/UIKyAKC0ab8",
   type: "video",
