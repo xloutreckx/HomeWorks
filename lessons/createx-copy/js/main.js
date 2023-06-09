1d956ccccb7978092fc390e6993f2a008866b71b
xloutreckx <133657705+xloutreckx@users.noreply.github.com>
2023-06-09 23:28:51 +1000

    Add files via upload

--- lessons/createx-copy/js/main.js
+++ lessons/createx-copy/js/main.js
@@ -1,14 +1,32 @@
-
 $(function () {
 
   var mixer = mixitup('.direction__items');
 
   $('.direction__filter-btn').on('click', function () {
-  $('.direction__filter-btn').removeClass('direction__filter-btn--active')
-  $(this).addClass('direction__filter-btn--active')
+    $('.direction__filter-btn').removeClass('direction__filter-btn--active')
+    $(this).addClass('direction__filter-btn--active')
   })
 
-})
+  $('.team__slider').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    infinite: true,
+    draggable: false,
+    waitForAnimate: false
+  })
+  $('.team__slider-prev').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickPrev')
+  })
+  $('.team__slider-next').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickNext')
+  })
+
+
+  })
+
 
 Fancybox.bind("[data-fancybox]", {
   src: "https://youtu.be/UIKyAKC0ab8",
@@ -17,4 +35,5 @@ Fancybox.bind("[data-fancybox]", {
   // width: 640,
   // height: 360,
 });
+
 
