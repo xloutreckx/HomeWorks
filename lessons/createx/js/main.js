a9d2b236cec856a82437e47537f8c6c346e9b0aa
xloutreckx <133657705+xloutreckx@users.noreply.github.com>
2023-06-16 22:35:09 +1000

    Add files via upload

--- lessons/createx/js/main.js
+++ lessons/createx/js/main.js
@@ -40,15 +40,44 @@ $(function () {
     e.preventDefault()
     $('.testimonials__slider').slick('slickNext')
   })
+
+// $('.program__acc-link').on('click', function (e) {
+//   e.preventDefault()
+//   $(this).toggleClass('program__acc-link--active')
+//   $(this).children('.program__acc-text').slideToggle()
+// })
+// $('.program__acc-link').on('click', function (e) {
+//   e.preventDefault()
+//   if ($(this).hasClass('program__acc-link--active')) {
+//     $(this).removeClass('program__acc-link--active')
+//     $(this).children('.program__acc-text').slideUp()
+//   } else {
+//     $('.program__acc-link').removeClass('program__acc-link--active')
+//     $('.program__acc-text').slideUp()
+//     $(this).addClass('program__acc-link--active')
+//     $(this).children('.program__acc-text').slideDowm()
+//   }
+// })
+
+$('.program__acc-link').on('click', function(e){
+  e.preventDefault()
+  if ($(this).hasClass('program__acc-link--active')) {
+      $(this).removeClass('program__acc-link--active')
+      $(this).children('.program__acc-text').slideUp()
+      stopImmediatePropagation()
+  }   
+  $('.program__acc-link').removeClass('program__acc-link--active')
+  $('.program__acc-text').slideUp()
+  $(this).toggleClass('program__acc-link--active')
+  $(this).children('.program__acc-text').slideDown()
+})
+
 })
 
 
 Fancybox.bind("[data-fancybox]", {
   src: "https://youtu.be/UIKyAKC0ab8",
   type: "video",
-  // ratio: 16 / 10,
-  // width: 640,
-  // height: 360,
 });
 
 
