673b846648fe003693182544d9eeeb02eeb06a4e
xloutreckx <133657705+xloutreckx@users.noreply.github.com>
2023-06-29 00:06:26 +1000

    Add files via upload

--- docs/lessons/createx/js/main.js
+++ docs/lessons/createx/js/main.js
@@ -101,14 +101,15 @@ $(function () {
       top = $(id).offset().top
     $('body,html').animate({ scrollTop: top }, 1500)
   })
+$(window).on('.scroll', function () {
+  if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+    $('.burger').addClass('burger--follow')
+  } else {
+    $('.burger').removeClass('burger--follow')
+  }
+})
 
-  setInterval(() => {
-    if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
-      $('.burger').addClass('burger--follow')
-    } else {
-      $('.burger').removeClass('burger--follow')
-    }
-  }, 0)
+
   $('.burger, .overlay, .header__top a').on('click', function (e) {
     e.preventDefault()
     $('.header__top').toggleClass('header__top--open')
