9d177391b5a93d4be73741f27aa5c14adb4df857
xloutreckx <133657705+xloutreckx@users.noreply.github.com>
2023-07-06 00:16:47 +1000

    Add files via upload

--- docs/lessons/final project/js/main.js
+++ docs/lessons/final project/js/main.js
@@ -19,4 +19,39 @@ $(function () {
     $('.services__slider').slick('slickNext')
   })
 
+  $('.review__slider').slick({
+    centerMode: true,
+    centerPadding: '-40px',
+    dots: false,
+    arrows: false,
+    slidesToShow: 3,
+    draggable: false,
+    infinite: true,
+    waitForAnimate: false,
+    appendDots: $('.review__dots')
+  })
+
+  $('.arrow__prev').on('click', function (e) {
+    e.preventDefault()
+    $('.review__slider').slick('slickPrev')
+  })
+  $('.arrow__next').on('click', function (e) {
+    e.preventDefault()
+    $('.review__slider').slick('slickNext')
+  })
+
+  $('.process__acc-link').on('click', function (e) {
+    e.preventDefault()
+    if ($(this).hasClass('process__acc-link--active')) {
+      $(this).removeClass('process__acc-link--active')
+      $(this).children('.process__acc-list').slideUp()
+    } else {
+      $('.process__acc-link').removeClass('process__acc-link--active')
+      $('.process__acc-list').slideUp()
+      $(this).addClass('process__acc-link--active')
+      $(this).children('.process__acc-list').slideDown()
+    }
+  })
+
+
 })
