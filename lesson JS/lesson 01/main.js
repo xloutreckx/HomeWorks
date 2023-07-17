13957b38fba121f3f6c84fd6b2fec349d4c13c04
xloutreckx <133657705+xloutreckx@users.noreply.github.com>
2023-07-17 23:35:22 +1000

    Add files via upload

---
+++ lesson JS/lesson 01/main.js
@@ -0,0 +1,11 @@
+const firstName = 'Sergey'
+const lastName = 'Marakhovsky'
+let age = 30
+let position = 'Development'
+let country = 'Russia'
+
+age = 34
+// lastName = 'Banana'
+
+console.log(firstName, lastName, age, 'y.o')
+console.log(position, country)
