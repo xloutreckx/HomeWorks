12d65a1ca0d60a271c96b02a080d0d941e97ec35
xloutreckx <133657705+xloutreckx@users.noreply.github.com>
2023-07-20 21:05:00 +1000

    Add files via upload

--- lesson JS/lesson 80/main.js
+++ lesson JS/lesson 80/main.js
@@ -1,49 +1,12 @@
-for (let i = 2; i <= 14; i = i + 2) {
-  switch (i) {
-    case 2:
-    case 4:
-      console.log(`пройдено ${i} цикла`);
-      break;
-
-    case 6:
-    case 8:
-    case 10:
-    case 12:
-      console.log(`пройдено ${i} циклов`);
-      break;
-
-      default:
-        console.log('Циклы закончены')
-  }
+for (let i = 2; i <= 12; i = i + 2) {
+  console.log(i)
 }
 
 // // ============================================================
 
 
-for (let i = 10; i >= 0; i = i - 1) {
-  switch (i) {
-    case 10:
-    case 9:
-    case 8:
-    case 7:
-    case 6:
-    case 5:
-      console.log(`осталось ${i} кругов`);
-      break;
-
-    case 4:
-    case 3:
-    case 2:
-      console.log(`осталось ${i} круга`);
-      break;
-
-    case 1:
-      console.log(`остался ${i} круг`);
-      break;
-
-      default:
-        console.log('Финиш')
-  }
+for (let i = 10; i >= 1; i = i - 1) {
+  console.log(i)
 }
 
 // // ============================================================
@@ -55,10 +18,10 @@ while(i < 12) {
   console.log(i)
 }
 
-let it = 11
+  i = 11
 
-while(it > 1) {
-  it = it - 1
-  console.log(it)
+while(i > 1) {
+  i = i - 1
+  console.log(i)
 }
 
