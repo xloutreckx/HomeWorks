9a6146f4d8ea6fc6577221b2aaf74a4a4435de55
xloutreckx <133657705+xloutreckx@users.noreply.github.com>
2023-08-09 23:00:37 +1000

    Add files via upload

--- docs/lessons/final project/js/main.js
+++ docs/lessons/final project/js/main.js
@@ -1,5 +1,33 @@
 (function () {
 
+  // Бургер
+
+  document.addEventListener('click', burgerInit)
+
+  function burgerInit(e) {
+    const burger = document.querySelector('.burger')
+    const innerHeader = document.querySelector('.header__inner')
+    const overlay = document.querySelector('.overlay')
+    const burgerIcon = e.target.closest('.burger')
+    const burgerNavLink = e.target.closest('.header__nav-link')
+
+    if (!burgerIcon && !burgerNavLink) return
+
+    if (!burger.classList.contains('burger--active')
+      && !innerHeader.classList.contains('header__inner--open')
+      && !overlay.classList.contains('overlay--show')) {
+      burger.classList.add('burger--active')
+      innerHeader.classList.add('header__inner--open')
+      overlay.classList.add('overlay--show')
+      document.body.classList.add('no-scroll')
+    } else {
+      burger.classList.remove('burger--active')
+      innerHeader.classList.remove('header__inner--open')
+      overlay.classList.remove('overlay--show')
+      document.body.classList.remove('no-scroll')
+    }
+  }
+
   // Слайдер секции "services"
 
   const swiperServices = new Swiper('.services__slider', {
@@ -25,7 +53,7 @@
     wrapperClass: 'services__slider-wrapper',
   })
 
-    // Слайдер секции "review"
+  // Слайдер секции "review"
 
   const swiperReview = new Swiper('.review__slider', {
 
@@ -49,41 +77,41 @@
     wrapperClass: 'review__slider-wrapper',
   })
 
-    // Аккордеон секции "process" и "questions"
+  // Аккордеон секции "process" и "questions"
 
-    const accordionLists = document.querySelectorAll('.accordion-list');
+  const accordionLists = document.querySelectorAll('.accordion-list');
 
-    accordionLists.forEach(el => {
+  accordionLists.forEach(el => {
 
-        el.addEventListener('click', (e) => {
+    el.addEventListener('click', (e) => {
 
-            const accordionList = e.currentTarget
-            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
-            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+      const accordionList = e.currentTarget
+      const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+      const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
 
-            const accordionControl = e.target.closest('.accordion-list__control');
-            if (!accordionControl) return
-            e.preventDefault()
-            const accordionItem = accordionControl.parentElement;
-            const accordionContent = accordionControl.nextElementSibling;
+      const accordionControl = e.target.closest('.accordion-list__control');
+      if (!accordionControl) return
+      e.preventDefault()
+      const accordionItem = accordionControl.parentElement;
+      const accordionContent = accordionControl.nextElementSibling;
 
-            if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
-                accordionOpenedItem.classList.remove('accordion-list__item--opened');
-                accordionOpenedContent.style.maxHeight = null;
-            }
-            accordionItem.classList.toggle('accordion-list__item--opened');
+      if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+        accordionOpenedItem.classList.remove('accordion-list__item--opened');
+        accordionOpenedContent.style.maxHeight = null;
+      }
+      accordionItem.classList.toggle('accordion-list__item--opened');
 
-            if (accordionItem.classList.contains('accordion-list__item--opened')) {
-                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
-            } else {
-                accordionContent.style.maxHeight = null;
-            }
-
-        });
+      if (accordionItem.classList.contains('accordion-list__item--opened')) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+      } else {
+        accordionContent.style.maxHeight = null;
+      }
 
     });
 
-    // Слайдер секции "compare"
+  });
+
+  // Слайдер секции "compare"
 
   const swiperCompare = new Swiper('.compare__slider', {
 
@@ -100,69 +128,69 @@
     wrapperClass: 'compare__slider-wrapper',
   })
 
-      // Слайдер секции "more"
+  // Слайдер секции "more"
 
-      const swiperMore = new Swiper('.more__slider', {
+  const swiperMore = new Swiper('.more__slider', {
 
-        slidesPerView: 3,
-        spaceBetween: 10,
-        loop: true,
-        loopedSlides: 3,
-    
-        navigation: {
-          nextEl: '.more__next',
-          prevEl: '.more__prev',
-        },
-    
-        slideClass: 'more__slide',
-        wrapperClass: 'more__slider-wrapper',
-      })
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true,
+    loopedSlides: 3,
 
-            // Слайдер секции "team"
+    navigation: {
+      nextEl: '.more__next',
+      prevEl: '.more__prev',
+    },
 
-            const swiperTeam = new Swiper('.team__slider', {
+    slideClass: 'more__slide',
+    wrapperClass: 'more__slider-wrapper',
+  })
 
-              slidesPerView: 3,
-              spaceBetween: 30,
+  // Слайдер секции "team"
 
-              pagination: {
-                el: '.team__dots',
-                clickable: true,
-              },
-          
-              navigation: {
-                nextEl: '.team__next',
-                prevEl: '.team__prev',
-              },
-          
-              slideClass: 'team__slide',
-              wrapperClass: 'team__slider-wrapper',
-            })
+  const swiperTeam = new Swiper('.team__slider', {
 
-            // Слайдер секции "team-cl"
+    slidesPerView: 3,
+    spaceBetween: 30,
 
-            const swiperTeamCl = new Swiper('.team__cl-slider', {
+    pagination: {
+      el: '.team__dots',
+      clickable: true,
+    },
 
-              slidesPerView: 3,
-              spaceBetween: 30,
-              centeredSlides: true,
-              loop: true,
-              loopedSlides: 3,
+    navigation: {
+      nextEl: '.team__next',
+      prevEl: '.team__prev',
+    },
 
-              pagination: {
-                el: '.team__cl-dots',
-                // clickable: true,
-              },
-          
-              navigation: {
-                nextEl: '.team__cl-next',
-                prevEl: '.team__cl-prev',
-              },
-          
-              slideClass: 'team__cl-slide',
-              wrapperClass: 'team__cl-slider-wrapper',
-            })
-            
+    slideClass: 'team__slide',
+    wrapperClass: 'team__slider-wrapper',
+  })
+
+  // Слайдер секции "team-cl"
+
+  const swiperTeamCl = new Swiper('.team__cl-slider', {
+
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    loopedSlides: 3,
+
+    pagination: {
+      el: '.team__cl-dots',
+      // clickable: true,
+    },
+
+    navigation: {
+      nextEl: '.team__cl-next',
+      prevEl: '.team__cl-prev',
+    },
+
+    slideClass: 'team__cl-slide',
+    wrapperClass: 'team__cl-slider-wrapper',
+  })
+
 })()
 
 // подключение Яндекс карты
